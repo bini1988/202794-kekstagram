bce54c43d5f7d43758c1897cf1aa9ae8e602582f
Nikolai <rus.nick@mail.ru>
2016-10-19 10:27:29 +0300

    Исправлены ошибки форматирования кода

--- src/js/check.js
+++ src/js/check.js
@@ -1,36 +1,39 @@
+'use strict';
 
 function getMessage(a, b) {
 
-	var outMessage = "Переданы некорректные данные";
+  var outMessage = 'Переданы некорректные данные';
 
-	if (typeof a === "boolean") {
-		outMessage = (a) 
-			? "Переданное GIF-изображение анимировано и содержит " + b + " кадров" 
-			: "Переданное GIF-изображение не анимировано"; 
-	}
+  if (typeof a === 'boolean') {
+    outMessage = (a)
+      ? 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров'
+      : 'Переданное GIF-изображение не анимировано';
+  }
 
-	if (typeof a === "number") {
-		outMessage = "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
-	}
+  if (typeof a === 'number') {
+    outMessage = 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
+  }
 
-	if (Array.isArray(a) && !Array.isArray(b)) {
+  if (Array.isArray(a) && !Array.isArray(b)) {
 
-		var amountOfRedPoints = a.reduce(function(pItem, cItem) { return pItem + cItem; });
+    var amountOfRedPoints = a.reduce(function(pItem, cItem) {
+      return pItem + cItem;
+    });
 
-		outMessage = "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
-	}
+    outMessage = 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
+  }
 
-	if (Array.isArray(a) && Array.isArray(b)) {
+  if (Array.isArray(a) && Array.isArray(b)) {
 
-		var artifactsSquare = 0;
+    var artifactsSquare = 0;
 
-		for(var i = 0; i < a.length; i++) {
-			artifactsSquare += a[i] * b[i];
-		}
+    for(var i = 0; i < a.length; i++) {
+      artifactsSquare += a[i] * b[i];
+    }
 
-		outMessage = "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
-	}
+    outMessage = 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+  }
 
-	return outMessage;
+  return outMessage;
 }
 
